778e0960ca22e92e0a2764cdc194df396c1fd73e
minminkimkim <minmin53733@gmail.com>
2020-12-22 00:43:55 -0600

    worked on some stuff

--- src/main/java/com/ballers/MyWebApp/HelloServlet.java
+++ src/main/java/com/ballers/MyWebApp/HelloServlet.java
@@ -1,5 +1,7 @@
 package com.ballers.MyWebApp;
 
+import jdk.incubator.jpackage.internal.Log;
+
 import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -9,12 +11,12 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "f java ";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-
+        System.out.println("Output: Hello servlet class");
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");

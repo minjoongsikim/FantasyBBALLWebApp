3a8c143e49cf6f9080ead145a86ffd5a67c0543c
minminkimkim <minmin53733@gmail.com>
2020-12-22 16:18:54 -0600

    working with Json

--- src/main/java/com/ballers/MyWebApp/ApiHandling.java
+++ src/main/java/com/ballers/MyWebApp/ApiHandling.java
@@ -1,10 +1,12 @@
 package com.ballers.MyWebApp;
-
+import javax.json.JsonObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import org.json.JSONObject;
+
 
 
 public class ApiHandling {
@@ -13,11 +15,16 @@ public class ApiHandling {
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        StringBuffer Response
         String inputLine = in.readLine();
+
         while ((inputLine != null)) {
-            System.out.println(inputLine);
+            response.append(inputLine);
         }
         in.close();
+
+        JSONObject myResponse = new JsonObject(response.toString()) {
+        }
     }
 
 

aaaf391bb77272f7bdc2be981488795c1990c9bb
lukechieng <lchieng2@illinois.edu>
2021-01-15 21:55:15 -0600

    Added BasketballReference WebScraper package and ensured functionality

--- src/main/webapp/web_scraper/main.py
+++ src/main/webapp/web_scraper/main.py
@@ -3,4 +3,5 @@ from basketball_reference_web_scraper.data import OutputType
 
 # Get all player season totals for the 2017-2018 season and output in JSON format
 print("Getting player season totals for 2020-2021 season in JSON format")
-print(client.players_season_totals(season_end_year=2021, output_type=OutputType.JSON))
+print(client.players_season_totals(season_end_year=2021, output_type=OutputType.JSON,
+                                   output_file_path="src/main/webapp/Resources/current_season_stats.json"))
